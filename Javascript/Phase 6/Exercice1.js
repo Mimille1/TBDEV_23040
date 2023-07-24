8b7bdaea8131d604d8756c267d434e3dee5471f8
Camille <camille.cliche1@gmail.com>
2023-07-24 16:56:47 +0200

    js 2

---
+++ Javascript/Phase 6/Exercice1.js
@@ -0,0 +1,9 @@
+var a = "100";
+var b = 100;
+var c = 1.00 ;
+var d = true ;
+
+window.alert("Ceci est une chaîne de caractères : " + a ) ;
+window.alert(b--) ;
+window.alert(c + a) ;
+window.alert(d = false) ;
