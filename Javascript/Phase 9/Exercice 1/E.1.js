9fd6181ce5f8bd631af4e5a3f15f070c4315ab7e
Camille <camille.cliche1@gmail.com>
2023-07-26 15:40:14 +0200

    9 1)

--- Javascript/Phase 9/Exercice 1/E.1.js
+++ Javascript/Phase 9/Exercice 1/E.1.js
@@ -1,11 +1,20 @@
-var x = 0 ;
-var y = 0 ;
+var x = 0;
+var y = 0;
+
+
+function cacheImage(papillon) {
+    document.getElementById("papillon").style.display = "none";
+}
 
 function produit(x, y) {
-    ...     
+    x = window.prompt("Entrez un nombre");
+    y = window.prompt("Entrez un multiplicateur");
+    var b = x * x * x
 }
+document.write("Le cube de " + x + " est" + b + "<br>")
+var a = x * y;
+document.write("Le produit de " + x + "x" + y + " est égal à " + calcul)
 
-function afficheImg(image) {
-    ...
+function afficheImg(papillon) {
+    document.getElementById("papillon").style.display = "block";
 }
-.innerHTML = ""
