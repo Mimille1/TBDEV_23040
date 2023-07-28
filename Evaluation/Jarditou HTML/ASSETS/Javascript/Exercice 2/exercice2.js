554b6dbd9a820ead9aa9d54d9756ac6bd08d7781
Camille <camille.cliche1@gmail.com>
2023-07-28 08:35:26 +0200

    eval ex 2

---
+++ Evaluation/Jarditou HTML/ASSETS/Javascript/Exercice 2/exercice2.js
@@ -0,0 +1,10 @@
+var multiple = window.prompt("Quelle table de multiplication souhaitez vous affichez ?");
+
+function TableMultiplication(a) {
+    for (let i = 0; i < 11; i++) {
+        var b = multiple * i;
+        document.write(i + "x" + multiple + "=" + b + "<br>");
+    }
+}
+
+TableMultiplication(multiple);
