7e52ac1d7123cab2c54df4c7092815c1c38d0197
Camille <camille.cliche1@gmail.com>
2023-07-28 10:45:47 +0200

    ex 2

--- Javascript/Evaluation/Exercice 2/exercice2.js
+++ Javascript/Evaluation/Exercice 2/exercice2.js
@@ -1,7 +1,7 @@
 var multiple = window.prompt("Quelle table de multiplication souhaitez vous affichez ?");
 
 function TableMultiplication(a) {
-    for (let i = 0; i < 11; i++) {
+    for (let i = 1; i < 11; i++) {
         var b = multiple * i;
         document.write(i + "x" + multiple + "=" + b + "<br>");
     }
