21e74cfc0158fd83c738eb5f062f15083db4e6e2
Camille <camille.cliche1@gmail.com>
2023-07-28 10:29:22 +0200

    eval js

---
+++ Evaluation/Jarditou HTML/ASSETS/Javascript/Exercice 5/evaluation.js
@@ -0,0 +1,46 @@
+form.querySelector('#button1').onclick = function (e) {
+    if (form.elements['nom'].value == '') {
+        window.alert('Veuillez renseigner votre nom.');
+        e.preventDefault();
+    }
+
+    if (form.elements['prénom'].value == '') {
+        window.alert('Veuillez renseigner votre prénom.');
+        e.preventDefault();
+    }
+
+    if (form.elements['sexef' + 'sexem'].value == '') {
+        window.alert('Veuillez renseigner votre sexe.');
+        e.preventDefault();
+    }
+
+    if (form.elements['date'].value == '') {
+        window.alert('Veuillez renseigner votre date de naissance.');
+        e.preventDefault();
+    }
+
+    if (form.elements['code'].value == '') {
+        window.alert('Veuillez renseigner votre code postal.');
+        e.preventDefault();
+    }
+
+    if (form.elements['email'].value == '') {
+        window.alert('Veuillez renseigner votre email.');
+        e.preventDefault();
+    }
+
+    if (form.elements['sujets'].value == '') {
+        window.alert('Veuillez renseigner votre sujets.');
+        e.preventDefault();
+    }
+
+    if (form.elements['question'].value == '') {
+        window.alert('Veuillez renseigner votre question.');
+        e.preventDefault();
+    }
+
+    if (form.elements['checkbox'].value == '') {
+        window.alert('Veuillez accepter le traitement informatique de ce formulaire.');
+        e.preventDefault();
+    }
+}
